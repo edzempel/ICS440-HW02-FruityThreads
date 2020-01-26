6b1d454dc708cce9d4d323dfc24c0a0d62a961ea
edzempel <edward.zempel@gmail.com>
2020-01-26 15:08:08 -0600

    add FruitDemo

---
+++ src/java/com/abc/fruit/FruitDemo.java
@@ -0,0 +1,10 @@
+package com.abc.fruit;
+
+public class FruitDemo {
+
+    public static void main(String[] args) {
+        Fruit orange = new Fruit("orange");
+        Fruit banana = new Fruit("banana");
+    }
+
+}
