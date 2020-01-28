9847c037678c1ffe5ebe092a11c2a6f93d7c4ec0
edzempel <edward.zempel@gmail.com>
2020-01-27 21:06:51 -0600

    suppressed unused warnings on self-running objects

--- src/java/com/abc/fruit/FruitDemo.java
+++ src/java/com/abc/fruit/FruitDemo.java
@@ -3,7 +3,9 @@ package com.abc.fruit;
 public class FruitDemo {
 
     public static void main(String[] args) {
+        @SuppressWarnings("unused")
         Fruit orange = new Fruit("orange");
+        @SuppressWarnings("unused")
         Fruit lemon = new Fruit("lemon");
     }
 
